919fda228187998d2675a8776b7eacc4bccccde0
Praveen Kumar <hellopraveensales@gmail.com>
2026-07-07 05:33:24 +0000

    Addint the __init__.py file in src folder.

---
+++ src/__init__.py
@@ -0,0 +1,22 @@
+"""
+Spam Email Detector - Source Code Package
+"""
+
+from .data_loader import DataLoader
+from .preprocessor import TextPreprocessor
+from .feature_extractor import FeatureExtractor
+from .trainer import ModelTrainer
+from .predictor import SpamPredictor
+from .utils import setup_logging, save_model, load_model
+
+__version__ = "1.0.0"
+__all__ = [
+    'DataLoader',
+    'TextPreprocessor',
+    'FeatureExtractor',
+    'ModelTrainer',
+    'SpamPredictor',
+    'setup_logging',
+    'save_model',
+    'load_model'
+]
